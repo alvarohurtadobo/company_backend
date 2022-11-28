19f9edb2af686878702ad504caa9919c82e1c8a6
alvarohurtadobo <a.hurtado.bo@gmail.com>
2022-11-27 23:26:08 -0400

    Solved small bug with double media folder

--- user/models.py
+++ user/models.py
@@ -25,7 +25,7 @@ class User(models.Model):
     password = models.CharField(max_length=256, verbose_name="Contraseña")
     first_name = models.CharField(max_length=256, verbose_name="Nombres")
     last_name = models.CharField(max_length=256, verbose_name="Apellidos")
-    photo_url = models.ImageField(upload_to="media/profiles/", default="media/profiles/default.png", verbose_name="Foto de perfil")
+    photo_url = models.ImageField(upload_to="profiles/", default="media/profiles/default.png", verbose_name="Foto de perfil")
     document = models.CharField(max_length=20, verbose_name="Documento", blank=True)
     city_id = models.ForeignKey(City, on_delete=models.RESTRICT, verbose_name="Ciudad")
     role_id = models.ForeignKey(Role, on_delete=models.RESTRICT, verbose_name="Rol")
